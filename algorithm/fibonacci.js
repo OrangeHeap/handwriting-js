578aacdaab994de07917aa16f41de9f9af8a620e
zhaoyucheng <zhaoyucheng@live.cn>
2020-06-27 18:59:32 +0800

    增加斐波那契实现方法

--- algorithm/fibonacci.js
+++ algorithm/fibonacci.js
@@ -1,5 +1,5 @@
 // 实现一个斐波那契数列
-function fibonacci(n) {
+function fibonacci01(n) {
   let dp = []
   dp[0] = 0
   dp[1] = 1
@@ -7,9 +7,25 @@ function fibonacci(n) {
 
   if (n <=2) return dp[n];
 
-  for (let i = 2; i < n; i++) {
-    dp[i] = dp[n-2] + dp[n-1]
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i-2] + dp[i-1]
   }
 
   return dp[n]
+}
+
+function fibonacci02 (n) {
+  let sum = 0;
+
+  if (n <= 1) return n
+
+  let a = 0; b = 1;
+
+  while (n-- > 1) {
+    sum = a + b
+    a = b;
+    b = sum;
+  }
+
+  return sum;
 }
